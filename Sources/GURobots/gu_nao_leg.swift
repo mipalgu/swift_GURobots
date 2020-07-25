a0a79633ae2e150657834305669d3be22755e2b0
Callum McColl <callum.mccoll1@gmail.com>
2020-07-26 04:37:13 +1000

    Created NaoLeg Wrapper

---
+++ Sources/GURobots/gu_nao_leg.swift
@@ -0,0 +1,96 @@
+/*
+ * gu_nao_leg.swift
+ * GURobots
+ *
+ * Created by Callum McColl on 26/7/20.
+ * Copyright © 2020 Callum McColl. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgement:
+ *
+ *        This product includes software developed by Callum McColl.
+ *
+ * 4. Neither the name of the author nor the names of contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * -----------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or
+ * modify it under the above terms or under the terms of the GNU
+ * General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+import CGURobots
+
+extension gu_nao_leg: Hashable, Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case hip
+        case knee
+        case ankle
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let hip = try values.decode(gu_yp_joint.self, forKey: .hip)
+        let knee = try values.decode(gu_pitch_joint.self, forKey: .knee)
+        let ankle = try values.decode(gu_pitch_roll_joint.self, forKey: .ankle)
+        self.init(hip: hip, knee: knee, ankle: ankle)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.hip, forKey: .hip)
+        try container.encode(self.knee, forKey: .knee)
+        try container.encode(self.ankle, forKey: .ankle)
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.hip)
+        hasher.combine(self.knee)
+        hasher.combine(self.ankle)
+    }
+    
+    public static func ==(lhs: gu_nao_leg, rhs: gu_nao_leg) -> Bool {
+        return lhs.hip == rhs.hip
+            && lhs.knee == rhs.knee
+            && lhs.ankle == rhs.ankle
+    }
+    
+}
